938d7186777618cdb6ec9178a7a8214f555319c7
canvasplay <canvasplay1@gmail.com>
2018-01-02 15:59:57 +0100

    set qunit.js as unit testing engine

--- test/JavaScript/test.js
+++ test/JavaScript/test.js
@@ -1,18 +1,19 @@
-var assert = require('assert');
-var pipwerks = require('../../src/JavaScript/SCORM_API_wrapper.js');
+QUnit.test("centisecsToISODuration", function (assert){
 
-try{
+    var t1 = 1513937992632;
+    var t2 = 1513938095752;
+    var d = t2 - t1;
 
-    let t1 = 1513937992632;
-    let t2 = 1513938095752;
-    let d = t2 - t1;
+    assert.equal(pipwerks.UTILS.centisecsToISODuration(d), 'PT17M11.2S');
 
-    assert.equal(pipwerks.UTILS.centisecsToISODuration(d),'PT17M11.2S');
-    assert.equal(pipwerks.UTILS.MillisecondsToCMIDuration(d),'00:01:43');
+});
 
-    console.log('All tests passed!');
+QUnit.test("MillisecondsToCMIDuration", function (assert) {
 
-}
-catch(e){
-    console.log(e);
-}
+    var t1 = 1513937992632;
+    var t2 = 1513938095752;
+    var d = t2 - t1;
+
+    assert.equal(pipwerks.UTILS.MillisecondsToCMIDuration(d), '00:01:43');
+
+});
